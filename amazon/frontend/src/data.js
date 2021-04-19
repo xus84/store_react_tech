c3332f7810afa06a946df9a8b29021bad07ed612
garsan84 <jgrsan84@gmail.com>
2021-04-19 17:53:07 +0200

    changing main folders

---
+++ amazon/frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [ 
+        {
+        _id: '1',
+        name: 'Mac computer',
+        category: 'technology',
+        image: '../products/macdesktop.jpg',
+        price: 1800,
+        countInStock: 10,
+        brand: 'Mac',
+        rating: 5 ,
+        description: 'high quality product'
+       
+       },
+       {
+        _id: '2',
+        name: 'Led Monitor',
+        category: 'technology',
+        image: '../products/ledmonitor.jpg',
+        price: 200,
+        countInStock: 10,
+        brand: 'Samsung',
+        rating: 4 ,
+        description: 'best seller'
+       
+       },
+       {
+        _id: '3',
+        name: 'key board',
+        category: 'technology',
+        image: '../products/keyboard.webp',
+        price: 120,
+        countInStock: 10,
+        brand: 'brand',
+        rating: 4,
+        description: 'high quality product'
+       
+       },
+       {
+        _id: '4',
+        name: 'mouse gamer',
+        category: 'technology',
+        image: '../products/mousegamer.jpg',
+        price: 25,
+        countInStock: 10,
+        brand: 'LG',
+        rating: 5 ,
+        description: 'best seller'
+       
+       },
+       {
+        _id: '5',
+        name: 'head phones',
+        category: 'technology',
+        image: '../products/sonyheadphones.jpg',
+        price: 30,
+        countInStock: 0,
+        brand: 'sony',
+        rating: 5 ,
+        description: 'high quality product'
+       
+       },
+       {
+        _id: '6',
+        name: 'chair',
+        category: 'technology',
+        image: '../products/chair.jpg',
+        price: 190,
+        countInStock: 10,
+        brand: 'Mac',
+        rating: 4 ,
+        description: 'high quality product'
+       
+       },
+  ], 
+};
+
+export default data;

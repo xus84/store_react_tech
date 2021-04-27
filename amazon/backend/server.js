1d9447673100607bff6159d78e81f251a8a94f9b
garsan84 <jgrsan84@gmail.com>
2021-04-27 12:53:04 +0200

    server.js and data.js added

--- amazon/backend/server.js
+++ amazon/backend/server.js
@@ -5,14 +5,12 @@ const app = express();
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
-})
+});
 
 app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+    res.send('server is ready');
+})
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Serve at http//localhost:${port}`);
+app.listen(5000, () => {
+    console.log('Server at http://localhost:5000');
 });
-

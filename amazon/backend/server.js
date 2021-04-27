1393165bc0342de5aa4a11ff12a95b653ae7da73
garsan84 <jgrsan84@gmail.com>
2021-04-27 18:52:49 +0200

    axios added

--- amazon/backend/server.js
+++ amazon/backend/server.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
-app.get('/', (req, res) => {
-    res.send('server is ready');
-})
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
 
-app.listen(5000, () => {
-    console.log('Server at http://localhost:5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server at http://localhost:5000${port}`);
 });
